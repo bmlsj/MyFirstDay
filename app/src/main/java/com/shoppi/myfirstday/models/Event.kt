1b07fc2f1d8d358f7fb99518aa6a2e2513decf2d
bmlsj <gaga2353@gmail.com>
2023-10-19 14:49:21 +0900

    feat: Event Data Class

---
+++ app/src/main/java/com/shoppi/myfirstday/models/Event.kt
@@ -0,0 +1,37 @@
+package com.shoppi.myfirstday.models
+
+class Event {
+
+    var type: String? = null
+        get() {
+            return field
+        }
+        set(value) {
+            field = value
+        }
+
+    var datetime: String? = null
+        get() {
+            return field
+        }
+        set(value) {
+            field = value
+        }
+
+    var location: String? = null
+        get() {
+            return field
+        }
+        set(value) {
+            field = value
+        }
+
+    var host: String? = null
+        get() {
+            return field
+        }
+        set(value) {
+            field = value
+        }
+
+}
